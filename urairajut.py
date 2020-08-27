073349785f59a5b9e4c59374844e3031b9999658
Hansenmarcelinoazali <70124501+Hansenmarcelinoazali@users.noreply.github.com>
2020-08-27 12:57:22 +0700

    Add files via upload
    ujian_module_1

---
+++ urairajut.py
@@ -0,0 +1,11 @@
+#urai
+def urai_rajut(str):
+    for i in range(len(str)): #menggunakan fusngi len untuk str
+        print(str[:i], end="") #setiap print akan mendapatkan printah selalu menambahkan dari satu sama lain sampai kalimat yang di tentukan habis atau selesai
+
+print(urai_rajut('code'))
+print(urai_rajut('purwadhika'))
+print(urai_rajut('python'))
+
+
+
