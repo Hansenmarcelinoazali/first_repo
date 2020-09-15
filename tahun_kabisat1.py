3851d8f056e68c4308c26cebc99999669e0d8020
Hansenmarcelinoazali <70124501+Hansenmarcelinoazali@users.noreply.github.com>
2020-09-15 15:16:06 +0700

    Add files via upload

---
+++ tahun_kabisat1.py
@@ -0,0 +1,12 @@
+year = int(input("masukan tahun: ")) #input angka (tahun) yang di inginkan
+#tahun kabisat adalah tahun dimana februari mempunyai tanggal 29 dan terjadi 4 tahun sekali
+
+# Leap Year Check
+if year % 4 == 0 and year % 100 != 0: #jika tahun yang di input modul0==4 dan hasilnya 0 maka tahun  tersebut adalah tahun kabisat
+    print(year, "Tahun Kabisat")
+elif year % 100 == 0: #jika tahun tersebut di modulo dengan 100 dan hasillnya tidak 0 maka tidak masuk dalam tahun kabisat
+    print(year, "Bukan Tahun Kabisat")
+elif year % 400 ==0: #jika tahun tersebut dibagi dengan 4 dan hasilnya 0 maka tahun tersebut adalah tahun kabisat
+    print(year, "Tahun Kabisat")
+else:
+    print(year, "bukan tahun kabisat") #selain dari itu adalah bukan tahun kabisat
